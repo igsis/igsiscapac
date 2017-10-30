a571c9ce15f6c5c5a9663f4712d6687ec312edb1
LoreleiGab <loreleigab@hotmail.com>
2017-10-30 11:04:31 -0200

    Cadastro de novos usuários

--- login_resultado.php
+++ login_resultado.php
@@ -5,12 +5,60 @@ require "funcoes/funcoesConecta.php";
 
 $con = bancoMysqli();
 
+if(isset($_POST['cadastrar']))
+{
+	$busca = $_POST['email'];
+	$num_busca = 0;
+	$telefone = $_POST['telefone'];
+	$nome = addslashes($_POST['nome']);
+	if($telefone == '' OR $nome == '')
+	{
+		$mensagem = "Por favor, preencha todos os campos.";
+	}
+	else
+	{
+		//verifica se há um post
+		if(($_POST['senha01'] != "") AND (strlen($_POST['senha01']) >= 5))
+		{
+			if($_POST['senha01'] == $_POST['senha02'])
+			{
+				$senha01 = md5($_POST['senha01']);
+				$email = $_POST['email'];
+				$idFraseSeguranca = $_POST['idFraseSeguranca'];
+				$respostaFrase = $_POST['respostaFrase'];
+				$sql_cadastra = "INSERT INTO `usuario`(`nome`, `email`, `senha`, `telefone`, `idFraseSeguranca`, `respostaFrase`) VALUES ('$nome', '$email', '$senha01', '$telefone', '$idFraseSeguranca', '$respostaFrase')";
+				$query_cadastra = mysqli_query($con,$sql_cadastra);
+				if($query_cadastra)
+				{
+					$mensagem = "<br/>Usuário cadastrado com sucesso! Aguarde que você será redirecionado para a página de login... <!--";
+					 echo "<script type=\"text/javascript\">
+						  window.setTimeout(\"location.href='index.php';\", 4000);
+						</script>";
+				}
+				else
+				{
+					$mensagem = "Erro ao cadastrar. Tente novamente.";
+				}
+			}
+			else
+			{
+				// caso não tenha digitado 2 vezes
+				$mensagem = "As senhas não conferem. Tente novamente.";
+			}
+		}
+		else
+		{
+			$mensagem = "A senha não pode estar em branco e deve conter mais de 5 caracteres";
+		}
+	}
+}
+
 if(isset($_POST['busca']))
 {
 	$validacao = validaEmail($_POST['busca']);
 	if($validacao == false)
 	{
-		echo "<meta HTTP-EQUIV='refresh' CONTENT='0;URL=erro_login.php'>";
+		echo "<meta HTTP-EQUIV='refresh' CONTENT='0;URL=erro_email.php'>";
 	}
 	else
 	{
@@ -57,7 +105,7 @@ if($num_busca > 0)
 											echo "
 												<tr>
 													<td class='list_description'><b>".$descricao['nome']."</b></td>
-													<td class='list_description'>".$descricao['login']."</td>
+													<td class='list_description'>".$descricao['email']."</td>
 													<td><a href='https://goo.gl/forms/AM7jU1XVDUBUVJXE3'><input type='submit' value='Esqueci a Senha' class='btn btn-theme btn-block'></a></td>
 												</tr>
 											";
@@ -85,17 +133,21 @@ if($num_busca > 0)
 else
 { // se não existir o cpf, imprime um formulário.
 	$busca = $_POST['busca'];
+	require "include/script.php";
 ?>
 	<html>
 		<head>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			<title>Mapeamento e Cadastro de Artistas e Profissionais de Arte e Cultura</title>
+			<title>IGSIS - Cadastro de Artistas e Profissionais de Arte e Cultura</title>
 			<link href="visual/css/bootstrap.min.css" rel="stylesheet" media="screen">
 			<link href="visual/css/style.css" rel="stylesheet" media="screen">
 			<link href="visual/color/default.css" rel="stylesheet" media="screen">
 			<script src="visual/js/modernizr.custom.js"></script>
 		</head>
 		<body>
+			<div id="bar">
+				<p id="p-bar">&nbsp;IGSIS - CADASTRO DE ARTISTAS E PROFISSIONAIS DE ARTE E CULTURA</p>
+			</div>
 			<section id="contact" class="home-section bg-white">
 				<div class="container">
 					<div class="form-group">
@@ -104,7 +156,7 @@ else
 					</div>
 					<div class="row">
 						<div class="col-md-offset-1 col-md-10">
-						<form class="form-horizontal" role="form" action="login_cadastro.php" method="post">
+						<form class="form-horizontal" role="form" action="login_resultado.php" method="post">
 							<div class="form-group">
 								<div class="col-md-offset-2 col-md-8"><strong>Nome: *</strong><br/>
 									<input type="text" class="form-control" name="nome" placeholder="Nome completo">
@@ -121,19 +173,32 @@ else
 							</div>
 
 							<div class="form-group">
-								<div class="col-md-offset-2 col-md-6"><strong>Telefone: *</strong><br/>
-									<input type="text" class="form-control" name="telefone" placeholder="(11) 98765-4321">
+								<div class="col-md-offset-2 col-md-6"><strong>Telefone *:</strong><br/>
+									<input type="text" class="form-control" name="telefone" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" placeholder="Exemplo: (11) 98765-4321" />
 								</div>
 								<div class="col-md-6"><strong>Email: *</strong><br/>
 									<input type="text" readonly class="form-control" name="email" value="<?php echo $busca ?>" placeholder="Email">
 								</div>
 							</div>
 
-							<!-- Botão para Gravar -->
+							<div class="form-group">
+								<div class="col-md-offset-2 col-md-8"><strong>Escolha uma pergunta secreta, para casos de recuperação de senha:</strong><br/>
+									<select class="form-control" name="idFraseSeguranca" id="idFraseSeguranca">
+										<option>Selecione...</option>
+										<?php geraOpcao("frase_seguranca","");	?>
+									</select>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-md-offset-2 col-md-8"><strong>Resposta:</strong><br/>
+									<input type="text" class="form-control" id="respostaFrase" maxlength="10" name="respostaFrase" />
+								</div>
+							</div>
+
 							<div class="form-group">
 								<div class="col-md-offset-2 col-md-8">
-									<input type="hidden" value="<?php echo $busca ?>">
-									<input type="hidden" name="cadastraR">
+									<input type="hidden" name="cadastrar">
 									<input type="submit" value="Enviar" class="btn btn-theme btn-lg btn-block">
 								</div>
 							</div>
@@ -142,10 +207,10 @@ else
 						</div>
 					</div>
 				</div>
-			</section> 
+			</section>
 			<?php include "visual/rodape.php" ?>
 		</body>
-	</html>	
-<?php	
+	</html>
+<?php
 }
 ?>
