1c8461215832e4197cdc0e90af3ec408323595d1
Lorelei Gabriele <loreleigab@hotmail.com>
2017-11-05 15:18:36 -0200

    Ajustes início

--- perfil/inicio.php
+++ perfil/inicio.php
@@ -1,28 +1,23 @@
-﻿<?php
-	$con = bancoMysqli();
-	$idPessoaFisica = $_SESSION['idUser'];
-	?>
-
-<section id="contact" class="home-section bg-white">
+﻿<section id="contact" class="home-section bg-white">
 	<div class="container">
 		<div class="form-group">
-			<h3>INFORMAÇÕES INICIAIS</h3>			
+			<h3>INFORMAÇÕES INICIAIS</h3>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
-			<div class="form-group">
-				<div class="col-md-offset-2 col-md-8">
-					<br />
-					<a href="?perfil=evento" class="btn btn-theme btn-lg btn-block" target="_blank" >EVENTO</a>
-					<br />
-					<a href="" class="btn btn-theme btn-lg btn-block" target="_blank" >PESSOA FÍSICA</a>
-					<br />
-					<a href="" class="btn btn-theme btn-lg btn-block" target="_blank" >PESSOA JURÍDICA</a>
-					<br />
-					<a href="?perfil=minha_conta" class="btn btn-theme btn-lg btn-block" target="_blank" >MINHA CONTA</a>
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<br />
+						<a href="?perfil=evento" class="btn btn-theme btn-lg btn-block" target="_blank" >EVENTO</a>
+						<br />
+						<a href="" class="btn btn-theme btn-lg btn-block" target="_blank" >PESSOA FÍSICA</a>
+						<br />
+						<a href="" class="btn btn-theme btn-lg btn-block" target="_blank" >PESSOA JURÍDICA</a>
+						<br />
+						<a href="?perfil=minha_conta" class="btn btn-theme btn-lg btn-block" target="_blank" >MINHA CONTA</a>
+					</div>
 				</div>
-			</div>			
 			</div>
 		</div>
 	</div>
-</section>  
+</section>
