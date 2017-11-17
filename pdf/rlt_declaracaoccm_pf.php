4f948a945f04a20ee04348981889ad15c6f981cd
Lorelei Gabriele <loreleigab@hotmail.com>
2017-11-17 13:16:07 -0200

    Correção nas variáveis da declaração de ccm PF e ajuste de layout

--- pdf/rlt_declaracaoccm_pf.php
+++ pdf/rlt_declaracaoccm_pf.php
@@ -1,19 +1,18 @@
-﻿<?php 
+﻿<?php
 
 // INSTALAÇÃO DA CLASSE NA PASTA FPDF.
-require_once("../include/lib/fpdf/fpdf.php");
 require_once("../funcoes/funcoesConecta.php");
 require_once("../funcoes/funcoesGerais.php");
 
-//CONEXÃO COM BANCO DE DADOS 
-$conexao = bancoMysqli(); 
-   
-session_start();
-   
-//CONSULTA 
-$idPessoaFisica = $_SESSION['idUser'];
+//CONEXÃO COM BANCO DE DADOS
+$conexao = bancoMysqli();
 
-$pessoa = recuperaDados("usuario_pf","id",$idPessoaFisica);
+session_start();
+
+//CONSULTA
+$idPf = $_SESSION['idPf'];
+
+$pessoa = recuperaDados("pessoa_fisica","id",$idPf);
 
 $Nome = $pessoa["nome"];
 $RG = $pessoa["rg"];
@@ -25,15 +24,15 @@ header("Content-Disposition: attachment;Filename=declaracao-ccm-$Nome.doc");
 echo "<html>";
 echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=Windows-1252\">";
 echo "<body>";
-echo 
+echo
 	"<p align='center'><strong>DECLARAÇÃO</strong></p>".
 	"<p>&nbsp;</p>".
-	"<p>Declaro, sob as penas da lei, que não sou inscrito no CCM de São Paulo, que não tenho débitos perante a Fazenda Pública Municipal de São Paulo no tocante a encargos tributários e que estou ciente de que o ISS sobre a operação será retido.</p>".
-	"<p>&nbsp;</p>".	
-	"<p>______________________________________</p>".
-	"<p><strong>Nome:</strong> ".$Nome."</p>".
-	"<p><strong>RG:</strong> ".$RG."</p>".
-	"<p><strong>CPF:</strong> ".$CPF."</p>";
+	"<p align='justify'>Declaro, sob as penas da lei, que não sou inscrito no CCM de São Paulo, que não tenho débitos perante a Fazenda Pública Municipal de São Paulo no tocante a encargos tributários e que estou ciente de que o ISS sobre a operação será retido.</p>".
+	"<p>&nbsp;</p>".
+	"<p>______________________________________________________</p>".
+	"<p><strong>Nome:</strong> ".$Nome."<br/>".
+	"<strong>RG:</strong> ".$RG."<br/>".
+	"<strong>CPF:</strong> ".$CPF."</p>";
 echo "</body>";
-echo "</html>";	
+echo "</html>";
 ?>
