be8b9efb2e8f6094103a4be584cf2739eee10b8d
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-01 15:40:04 -0300

    Descrição do menu

--- perfil/inicio.php
+++ perfil/inicio.php
@@ -12,12 +12,16 @@ unset($_SESSION['idPf']);
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-8">
 						<br />
+						<p>Aqui são inseridas as informações sobre o seu evento.</p>
 						<a href="?perfil=evento_apresentacao" class="btn btn-theme btn-lg btn-block">EVENTO</a>
 						<br />
+						<p>Aqui você cadastra o artista que executará o evento.</p>
 						<a href="?perfil=proponente_pf" class="btn btn-theme btn-lg btn-block">PESSOA FÍSICA</a>
 						<br />
+						<p>Aqui você cadastra os dados da empresa que representará o artista.</p>
 						<a href="?perfil=proponente_pj" class="btn btn-theme btn-lg btn-block">PESSOA JURÍDICA</a>
 						<br />
+						<p>Aqui você altera os seus dados de login e senha.</p>
 						<a href="?perfil=minha_conta" class="btn btn-theme btn-lg btn-block" target="_blank" >MINHA CONTA</a>
 						<br />
 					</div>
