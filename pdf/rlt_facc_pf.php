265567c13adf9261ac99b0f91aa8054bc543003d
LoreleiGab <loreleigab@hotmail.com>
2018-03-01 16:44:22 -0300

    Alterações no cadastro de endereço para gerar FACC corretamente

--- pdf/rlt_facc_pf.php
+++ pdf/rlt_facc_pf.php
@@ -26,21 +26,19 @@ class PDF extends FPDF
 $idPf = $_SESSION['idPf'];
 
 $pessoa = recuperaDados("pessoa_fisica","id",$idPf);
-$enderecoCEP = enderecoCEP($pessoa['cep']);
 $dbBanco = recuperaDados("banco","id",$pessoa['codigoBanco']);
 
 $banco = $dbBanco["banco"];
 $codbanco = $dbBanco["codigoBanco"];
 
-$rua = $enderecoCEP["rua"];
-$bairro = $enderecoCEP["bairro"];
-$cidade = $enderecoCEP["cidade"];
-$estado = $enderecoCEP["estado"];
-
 $Nome = $pessoa["nome"];
 $RG = $pessoa["rg"];
 $CPF = $pessoa["cpf"];
 $CCM = $pessoa["ccm"];
+$rua = $pessoa['logradouro'];
+$bairro = $pessoa["bairro"];
+$cidade = $pessoa["cidade"];
+$estado = $pessoa["estado"];
 $NumEndereco = $pessoa["numero"];
 $Complemento = $pessoa["complemento"];
 $cep = $pessoa["cep"];
@@ -69,25 +67,25 @@ $l=7; //DEFINE A ALTURA DA LINHA
    $pdf->SetXY($x, 40);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(53,$l,utf8_decode($CPF),0,0,'L');
-   
+
    $pdf->SetXY(155, 40);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(53,$l,utf8_decode($CCM),0,0,'L');
-   
+
    $pdf->SetXY($x, 55);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(160,$l,utf8_decode($Nome),0,0,'L');
-   
+
    $pdf->SetXY($x, 68);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(160,$l,utf8_decode("$rua".", "."$NumEndereco".", "."$Complemento"),0,0,'L');
-   
+
    $pdf->SetXY($x, 82);
    $pdf->SetFont('Arial','', 9);
    $pdf->Cell(68,$l,utf8_decode($bairro),0,0,'L');
    $pdf->Cell(88,$l,utf8_decode($cidade),0,0,'L');
    $pdf->Cell(5,$l,utf8_decode($estado),0,0,'L');
-   
+
    $pdf->SetXY($x, 96);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(33,$l,utf8_decode($cep),0,0,'L');
@@ -95,18 +93,17 @@ $l=7; //DEFINE A ALTURA DA LINHA
    $pdf->Cell(15,$l,utf8_decode($codbanco),0,0,'L');
    $pdf->Cell(35,$l,utf8_decode($agencia),0,0,'L');
    $pdf->Cell(37,$l,utf8_decode($conta),0,0,'L');
-   
+
    $pdf->SetXY($x, 107);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(87,$l,utf8_decode($PIS),0,0,'L');
    $pdf->Cell(52,$l,utf8_decode($DataNascimento),0,0,'L');
    $pdf->Cell(33,$l,utf8_decode($cbo),0,0,'L');
-   
+
    $pdf->SetXY($x, 122);
    $pdf->SetFont('Arial','', 9);
    $pdf->Cell(87,$l,utf8_decode($Nome),0,0,'L');
    $pdf->Cell(50,$l,utf8_decode($RG),0,0,'L');
-
 
 $pdf->Output("FACC - ".$Nome.".pdf", "I");
 ?>
