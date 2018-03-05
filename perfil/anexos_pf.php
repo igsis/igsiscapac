2b2bb9e14b05a67376aebe521f9d7cb1ed82bce3
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-05 10:41:16 -0300

    Erro de mensagem - Arquivos PF

--- perfil/anexos_pf.php
+++ perfil/anexos_pf.php
@@ -45,7 +45,7 @@ if(isset($_POST["enviar"]))
 						$query = mysqli_query($con,$sql_insere_arquivo);
 						if($query)
 						{
-							$mensagem = "<font color='#FF0000'><strong>Erro ao gravar no banco!</strong></font>";
+							$mensagem = "<font color='#01DF3A'><strong>Arquivo recebido com sucesso!</strong></font>";
 							gravarLog($sql_insere_arquivo);
 						}
 						else
