123510d9cca4cab2633e6acb50e0bd13e5ad829b
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-09 12:23:20 -0300

    Listar arquivos - PF e PJ

--- perfil/final_pf.php
+++ perfil/final_pf.php
@@ -5,12 +5,117 @@ $idPf = $_SESSION['idPf'];
 $pf = recuperaDados("pessoa_fisica","id",$idPf);
 $contador = 0;
 
+function recuperaBanco($campoY)
+{
+	$banco = recuperaDados("banco","id",$campoY);
+	$nomeBanco = $banco['banco'];
+	return $nomeBanco;
+}
+
+function listaArquivoCamposMultiplos1($idPessoa,$pf)
+{
+	$con = bancoMysqli();
+	switch ($pf) {
+		case 1: //todos os arquivos de pf
+			$sql = "SELECT *
+				FROM upload_lista_documento as list
+				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
+				WHERE arq.idPessoa = '$idPessoa'
+				AND arq.idTipoPessoa = '1'
+				AND arq.publicado = '1'
+				ORDER BY documento";
+		break;
+		case 2: //todos os arquivos de pj
+			$sql = "SELECT *
+				FROM upload_lista_documento as list
+				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
+				WHERE arq.idPessoa = '$idPessoa'
+				AND arq.idTipoPessoa = '2'
+				AND arq.publicado = '1'
+				AND list.id NOT IN (20,21,103,104)
+				ORDER BY documento";
+		break;
+		case 3: //representante_legal1
+			$arq1 = "AND (list.id = '20' OR ";
+			$arq2 = "list.id = '21'OR ";
+			$arq3 = "list.id = '103' OR ";
+			$arq4 = "list.id = '104')";
+			$sql = "SELECT *
+				FROM upload_lista_documento as list
+				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
+				WHERE arq.idPessoa = '$idPessoa'
+				AND arq.idTipoPessoa = '2'
+				$arq1 $arq2 $arq3 $arq4
+				AND arq.publicado = '1'";
+		break;
+		case 4: //grupo
+			$arq1 = "AND (list.id = '99' OR ";
+			$arq2 = "list.id = '100' OR";
+			$arq3 = "list.id = '101' OR";
+			$arq4 = "list.id = '102')";
+			$sql = "SELECT *
+				FROM upload_lista_documento as list
+				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
+				WHERE arq.idPessoa = '$idPessoa'
+				AND arq.idTipoPessoa = '3'
+				$arq1 $arq2 $arq3 $arq4
+				AND arq.publicado = '1'";
+		break;
+		case 5: //evento
+			$arq1 = "AND (list.id = '23' OR ";
+			$arq2 = "list.id = '65' OR";
+			$arq3 = "list.id = '78' OR";
+			$arq4 = "list.id = '96' OR";
+			$arq5 = "list.id = '97' OR";
+			$arq6 = "list.id = '101')";
+			$sql = "SELECT *
+				FROM upload_lista_documento as list
+				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
+				WHERE arq.idPessoa = '$idPessoa'
+				AND arq.idTipoPessoa = '3'
+				$arq1 $arq2 $arq3 $arq4 $arq5 $arq6
+				AND arq.publicado = '1'";
+		break;
+		default:
+		break;
+	}
+	$query = mysqli_query($con,$sql);
+	$linhas = mysqli_num_rows($query);
+
+	if ($linhas > 0)
+	{
+	echo "
+		<table class='table table-condensed'>
+			<thead>
+				<tr class='list_menu'>
+					<td>Nome do arquivo</td>
+					<td width='10%'></td>
+				</tr>
+			</thead>
+			<tbody>";
+				while($arquivo = mysqli_fetch_array($query))
+				{
+					echo "<tr>";
+					echo "<td class='list_description' width='5%'>".$arquivo['documento']."</td>";
+					echo "<td class='list_description'><a href='../../igsiscapac/uploadsdocs/".$arquivo['arquivo']."' target='_blank'>".$arquivo['arquivo']."</td>";
+					echo "</tr>";
+				}
+				echo "
+		</tbody>
+		</table>";
+	}
+	else
+	{
+		echo "<p>Não há arquivo(s) inserido(s).<p/><br/>";
+	}
+}
+
 ?>
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_evento.php'; ?>
 		<div class="form-group">
 			<h4>Finalizar</h4>
-	<p><strong><font color="green">Todos os campos obrigatórios foram preenchidos corretamente.</font></strong></p><br>
+	<p><strong><font color="green">Todos os campos obrigatórios foram preenchidos corretamente.<br/> Seu cadastro de Pessoa Física foi concluído com sucesso!</font></strong></p><br>
 	<div class="container">
 		 <div class = "page-header"> <h5>Informações Pessoais </h5><br></div>
 		 <div class="well">
@@ -43,9 +148,13 @@ $contador = 0;
 		<div class = "page-header"><h5>Informações Complementares: </h5><br></div>
 		<div class="well">
 			<p align="justify"><strong>DRT:</strong> <?php echo $pf['drt']; ?></p>
-			<p align="justify"><strong>Banco:</strong> <?php echo $pf['codigoBanco']; ?></p>
+			<p align="justify"><strong>Banco:</strong> <?php echo recuperaBanco ($pf['codigoBanco']); ?></p>
 			<p align="justify"><strong>Agência:</strong> <?php echo $pf['agencia']; ?></p>
 			<p align="justify"><strong>Conta:</strong> <?php echo $pf['conta']; ?></p>
 	</div>
+	</div>
+
+	<div class="table-responsive list_info"><h6>Arquivo(s) de Pessoa Física</h6>
+		<?php listaArquivoCamposMultiplos1($pf['id'],1); ?>
 	</div>
 </section>
