095581c3b83e3b4cc593b31f5a2e56e1c51e3e68
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-12 11:36:46 -0300

    Ajuste menu arquivos_representante1

--- perfil/arquivos_representante1.php
+++ perfil/arquivos_representante1.php
@@ -227,7 +227,7 @@ $pj = recuperaDados("pessoa_juridica","id",$idPj);
 				<!-- Botão para Voltar e Prosseguir -->
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-2">
-						<form class="form-horizontal" role="form" action="?perfil=endereco_pj" method="post">
+						<form class="form-horizontal" role="form" action="?perfil=representante1_pj_cadastro" method="post">
 							<input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPj ?>">
 						</form>
 					</div>
