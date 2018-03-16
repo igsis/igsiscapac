789950a45672032ac3c17787252b413bb701a129
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-16 11:16:32 -0300

    Divisão - dados Bancários

--- perfil/arquivos_dados_bancarios_pf.php
+++ perfil/arquivos_dados_bancarios_pf.php
@@ -1,34 +1,9 @@
-﻿<?php
+<?php
 $con = bancoMysqli();
 $idPf = $_SESSION['idPf'];
 $evento = isset($_SESSION['idEvento']) ? $_SESSION['idEvento'] : null;
 $idCampo = 51;
 $tipoPessoa = 1;
-
-if(isset($_POST['cadastrarBanco']))
-{
-	$idPf = $_POST['cadastrarBanco'];
-	$CodigoBanco = $_POST['codigoBanco'];
-	$Agencia = $_POST['agencia'];
-	$Conta = $_POST['conta'];
-
-	$sql_atualiza_pf = "UPDATE pessoa_fisica SET
-	`codigoBanco` = '$CodigoBanco',
-	`agencia` = '$Agencia',
-	`conta` = '$Conta'
-	WHERE `id` = '$idPf'";
-
-	if(mysqli_query($con,$sql_atualiza_pf))
-	{
-		$mensagem = "<font color='#01DF3A'><strong>Atualizado com sucesso!</strong></font>";
-		gravarLog($sql_atualiza_pf);
-	}
-	else
-	{
-		$mensagem = "<font color='#FF0000'><strong>Erro ao atualizar! Tente novamente.</strong></font>";
-	}
-}
-
 
 if(isset($_POST["enviar"]))
 {
@@ -112,44 +87,107 @@ $pf = recuperaDados("pessoa_fisica","id",$idPf);
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_evento.php'; ?>
 		<div class="form-group">
-			<h3>Dados Bancários</h3>
+			<h3>Arquivos Dados Bancários</h3>
 			<p><b>Código de cadastro:</b> <?php echo $idPf; ?> | <b>Nome:</b> <?php echo $pf['nome']; ?></p>
 			<h5><?php if(isset($mensagem)){echo $mensagem;};?></h5>
 		</div>
-		<div class="row">
-			<div class="col-md-offset-1 col-md-10">
-			<form class="form-horizontal" role="form" action="?perfil=dados_bancarios_pf" method="post">
 
-			<font color="#FF0000"><strong>Realizamos pagamentos de valores acima de R$ 5.000,00 *SOMENTE COM CONTA CORRENTE NO BANCO DO BRASIL*.<br />
-			Não são aceitas: conta fácil, poupança e conjunta.</strong></font><br />
-			<p>
-
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8"><strong>Banco:</strong><br/>
-						<select class="form-control" name="codigoBanco" id="codigoBanco">
-							<option></option>
-							<?php geraOpcaoBancos("banco",$pf['codigoBanco'],"");	?>
-						</select>
-					</div>
-				</div>
-
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-6"><strong>Agência</strong><br/>
-						<input type="text" class="form-control" id="agencia" name="agencia" placeholder="" maxlength="12" value="<?php echo $pf['agencia']; ?>">
-					</div>
-					<div class=" col-md-6"><strong>Conta:</strong><br/>
-						<input type="text" class="form-control" id="conta" name="conta" placeholder="" maxlength="12" value="<?php echo $pf['conta']; ?>">
-					</div>
-				</div>
+				<!-- Gerar FACC -->
+				<?php
+					$server = "http://".$_SERVER['SERVER_NAME']."/igsiscapac/";
+					$http = $server."/pdf/";
+					$link1 = $http."rlt_facc_pf.php"."?id_pf=".$idPf;
+				?>
 
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-8">
-						<input type="hidden" name="cadastrarBanco" value="<?php echo $idPf ?>">
-						<input type="submit" value="GRAVAR" class="btn btn-theme btn-lg btn-block">
+						<p align="justify">A FACC - Ficha de Atualização de Cadastro de Credores é um documento necessário para recebimento do cachê.</p>
 					</div>
 				</div>
-			</form>
-			
+
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-5">
+						<p align="left">Após inserir seus dados pessoais e os dados bancários, clique no botão para gerar a FACC.</p>
+					</div>
+					<div class="col-md-3">
+						<a href='<?php echo $link1 ?>' target='_blank' class="btn btn-theme btn-lg btn-block"><strong>Gerar</strong></a>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<p align="justify"><font color="red"><strong>A FACC deve ser impressa, datada e assinada nos campos indicados no documento. Logo após, deve-se digitaliza-la e então anexa-la ao sistema através do campo abaixo.</strong></font></p>
+					</div>
+				</div>
+				<!--  FIM Gerar FACC -->
+
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8"><hr/><br/></div>
+				</div>
+
+				<!-- Exibir arquivos -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class="table-responsive list_info"><h6>Arquivo(s) Anexado(s)</h6>
+							<?php listaArquivoCamposMultiplos($idPf,$tipoPessoa,$idCampo,"dados_bancarios_pf",3); ?>
+						</div>
+					</div>
+				</div>
+
+				<!-- Upload de arquivo -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class = "center">
+						<form method="POST" action="?perfil=dados_bancarios_pf" enctype="multipart/form-data">
+							<table>
+								<tr>
+									<td width="50%"><td>
+								</tr>
+								<?php
+								if(verificaArquivosExistentesPF($idPf,'51')){
+										echo '<div class="alert alert-success">O arquivo FACC foi enviado.</div> ';
+									}
+									else{
+									$sql_arquivos = "SELECT * FROM upload_lista_documento WHERE idTipoUpload = '$tipoPessoa' AND id = '$idCampo'";
+									$query_arquivos = mysqli_query($con,$sql_arquivos);
+									while($arq = mysqli_fetch_array($query_arquivos))
+									{
+								?>
+										<tr>
+											<td><label><?php echo $arq['documento']?></label></td><td><input type='file' name='arquivo[<?php echo $arq['sigla']; ?>]'></td>
+										</tr>
+								<?php
+									}
+								}
+								?>
+							</table><br>
+							<input type="hidden" name="idPessoa" value="<?php echo $idPf; ?>"  />
+							<input type="hidden" name="tipoPessoa" value="<?php echo $tipoPessoa; ?>"  />
+							<input type="submit" name="enviar" class="btn btn-theme btn-lg btn-block" value='Enviar'>
+						</form>
+						</div>
+					</div>
+				</div>
+				<!-- Fim Upload de arquivo -->
+				<!-- Confirmação de Exclusão -->
+					<div class="modal fade" id="confirmApagar" role="dialog" aria-labelledby="confirmApagarLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h4 class="modal-title">Excluir Arquivo?</h4>
+								</div>
+								<div class="modal-body">
+									<p>Confirma?</p>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+									<button type="button" class="btn btn-danger" id="confirm">Apagar</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				<!-- Fim Confirmação de Exclusão -->
+
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-8"><hr/><br/></div>
 				</div>
@@ -157,18 +195,15 @@ $pf = recuperaDados("pessoa_fisica","id",$idPf);
 				<!-- Botão para Voltar e Prosseguir -->
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-2">
-						<form class="form-horizontal" role="form" action="?perfil=informacoes_complementares_pf" method="post">
+						<form class="form-horizontal" role="form" action="?perfil=dados_bancarios_pf" method="post">
 							<input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPf ?>">
 						</form>
 					</div>
 					<div class="col-md-offset-4 col-md-2">
-						<form class="form-horizontal" role="form" action="?perfil=arquivos_dados_bancarios_pf" method="post">
+						<form class="form-horizontal" role="form" action="?perfil=anexos_pf" method="post">
 							<input type="submit" value="Avançar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPf ?>">
 						</form>
 					</div>
 				</div>
-
-			</div>
-		</div>
 	</div>
-</section>
+</section>	
