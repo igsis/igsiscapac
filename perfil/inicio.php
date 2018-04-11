e913b81c65d983454c921bf32d4cd55683feb261
LoreleiGab <loreleigab@hotmail.com>
2018-04-11 16:06:23 -0300

    Mudanças na tela inicial do sistema

--- perfil/inicio.php
+++ perfil/inicio.php
@@ -11,7 +11,10 @@ unset($_SESSION['idPf']);
 			<div class="col-md-offset-1 col-md-10">
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-8">
-						<br />
+						<div class="alert alert-danger">
+							<h5><font color="red">ATENÇÃO!</font></h5>
+							<p><font color="red">Para gerar o código do CAPAC é necessário preencher a área de EVENTO.</font></p>
+						</div>
 						<?php
 							$usr = $_SESSION['idUser'];
 							if($usr == 18 || $usr == 19 || $usr = 54 || $usr = 55 || $usr = 204)
@@ -19,13 +22,13 @@ unset($_SESSION['idPf']);
 								echo "<a href='?perfil=busca_reset' class='btn btn-theme btn-lg btn-block'>Reiniciar Senha</a><br/>";
 							}
 						?>
-						<p>Aqui são inseridas as informações sobre o seu evento.</p>
+						<p>Aqui são inseridas as informações sobre o seu evento, incluindo pessoa jurídica e/ou física.</p>
 						<a href="?perfil=evento_apresentacao" class="btn btn-theme btn-lg btn-block">EVENTO</a>
 						<br />
-						<p>Aqui você cadastra o artista que executará o evento.</p>
+						<p>Aqui você atualiza o cadastro do artista.</p>
 						<a href="?perfil=proponente_pf" class="btn btn-theme btn-lg btn-block">PESSOA FÍSICA</a>
 						<br />
-						<p>Aqui você cadastra os dados da empresa que representará o artista.</p>
+						<p>Aqui você atualiza os dados cadastrais da empresa.</p>
 						<a href="?perfil=proponente_pj" class="btn btn-theme btn-lg btn-block">PESSOA JURÍDICA</a>
 						<br />
 						<p>Aqui você altera os seus dados de login e senha.</p>
