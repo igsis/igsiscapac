559c88693370acfc6f5e80d4fffe372a8144f60f
José Campillo <jose.campillo@hotmail.com>
2018-04-13 16:41:07 -0300

    Alteração na listagem de arquivos do evento

--- perfil/finalizar.php
+++ perfil/finalizar.php
@@ -68,20 +68,13 @@ function listaArquivoCamposMultiplos1($idPessoa,$pf)
 				AND arq.publicado = '1'";
 		break;
 		case 5: //evento
-			$arq1 = "AND (list.id = '23' OR ";
-			$arq2 = "list.id = '65' OR";
-			$arq3 = "list.id = '78' OR";
-			$arq4 = "list.id = '96' OR";
-			$arq5 = "list.id = '97' OR";
-			$arq6 = "list.id = '98' OR";
-			$arq7 = "list.id = '105')";
 			$sql = "SELECT *
 				FROM upload_lista_documento as list
 				INNER JOIN upload_arquivo as arq ON arq.idUploadListaDocumento = list.id
 				WHERE arq.idPessoa = '$idPessoa'
 				AND arq.idTipoPessoa = '3'
-				$arq1 $arq2 $arq3 $arq4 $arq5 $arq6 $arq7
-				AND arq.publicado = '1'";
+				AND arq.publicado = '1'
+				ORDER BY documento";
 		break;
 		default:
 		break;
