e8b579cc5c8626a219291804023e7e30e4c2d6b3
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-04-17 12:33:23 -0300

    Demais Anexos - Ajuste no título da página

--- perfil/anexos_pf.php
+++ perfil/anexos_pf.php
@@ -138,9 +138,9 @@ $pf = recuperaDados("pessoa_fisica","id",$idPf);
 	<div class="container"><?php include 'includes/menu_evento.php'; ?>
 		<div class="form-group">
 			<?php if($evento == NULL || $evento == ""){ ?>
-			<h3>PASSO 11: Demais Anexos</h3>
+			<h3>Demais Anexos</h3>
 			<?php } else { ?>
-			<h3>PASSO 6: Demais Anexos</h3>
+			<h3>Demais Anexos</h3>
 			<?php } ?>
 			<p><b>Código de cadastro:</b> <?php echo $idPf; ?> | <b>Nome:</b> <?php echo $pf['nome']; ?></p>
 			<h5><?php if(isset($mensagem)){echo $mensagem;}; ?></h5>
--- perfil/anexos_pj.php
+++ perfil/anexos_pj.php
@@ -133,7 +133,7 @@ $pj = recuperaDados("pessoa_juridica","id",$idPj);
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_evento.php'; ?>
 		<div class="form-group">
-			<h4>PASSO 15: Demais Anexosss</h4>
+			<h4> Demais Anexos</h4>
 			<p><b>Razão Social:</b> <?php echo $pj['razaoSocial']; ?></p>
 			<h5><?php if(isset($mensagem)){echo $mensagem;}; ?></h5>
 		</div>
