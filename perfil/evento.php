8927078db3befe9c4c631ac82d07c69f7baaa024
Henrique Tinem <henrique714tinem@gmail.com>
2018-07-18 14:53:30 -0300

    confirmação de exclusao

--- perfil/evento.php
+++ perfil/evento.php
@@ -22,44 +22,47 @@ if(isset($_POST['apagar']))
 
 $usuario = recuperaDados("usuario","id",$idUser);
 ?>
-<section id="list_items" class="home-section bg-white">
-	<div class="container"><?php include '../perfil/includes/menu_evento.php'; ?>
-		<div class="form-group">
-			<h3>EVENTOS</h3>
-			<h5><?php if(isset($mensagem)){echo $mensagem;}; ?></h5>
-		</div>
-		<div class="row">
-			<div class="col-md-offset-1 col-md-10">
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8">
-						<form class="form-horizontal" role="form" action="?perfil=evento_novo" method="post">
-							<input type="submit" value="Inserir novo evento com cachê" class="btn btn-theme btn-lg btn-block">
-						</form>
-					</div>
-				</div>
-				<br/>
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8">
-						<form class="form-horizontal" role="form" action="?perfil=evento_semcache_novo" method="post">
-							<input type="submit" value="Inserir novo evento sem cachê" class="btn btn-theme btn-lg btn-block">
-						</form>
-					</div>
-				</div>
-				<br/>
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8">
-						<form class="form-horizontal" role="form" action="?perfil=evento_semcontratacao_novo" method="post">
-							<input type="submit" value="Inserir novo evento sem contratação" class="btn btn-theme btn-lg btn-block">
-						</form>
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-md-offset-2 col-md-8"><br></div>
-			</div>
-			<div class="col-md-offset-1 col-md-10">
-				<div class="table-responsive list_info">
-				<?php
+    <section id="list_items" class="home-section bg-white">
+        <div class="container">
+            <?php include '../perfil/includes/menu_evento.php'; ?>
+            <div class="form-group">
+                <h3>EVENTOS</h3>
+                <h5>
+                    <?php if(isset($mensagem)){echo $mensagem;}; ?>
+                </h5>
+            </div>
+            <div class="row">
+                <div class="col-md-offset-1 col-md-10">
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-8">
+                            <form class="form-horizontal" role="form" action="?perfil=evento_novo" method="post">
+                                <input type="submit" value="Inserir novo evento com cachê" class="btn btn-theme btn-lg btn-block">
+                            </form>
+                        </div>
+                    </div>
+                    <br/>
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-8">
+                            <form class="form-horizontal" role="form" action="?perfil=evento_semcache_novo" method="post">
+                                <input type="submit" value="Inserir novo evento sem cachê" class="btn btn-theme btn-lg btn-block">
+                            </form>
+                        </div>
+                    </div>
+                    <br/>
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-8">
+                            <form class="form-horizontal" role="form" action="?perfil=evento_semcontratacao_novo" method="post">
+                                <input type="submit" value="Inserir novo evento sem contratação" class="btn btn-theme btn-lg btn-block">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-offset-2 col-md-8"><br></div>
+                </div>
+                <div class="col-md-offset-1 col-md-10">
+                    <div class="table-responsive list_info">
+                        <?php
 					$sql = "SELECT * FROM evento
 							WHERE publicado > 0 AND idUsuario ='$idUser'
 							ORDER BY id DESC";
@@ -165,7 +168,8 @@ $usuario = recuperaDados("usuario","id",$idUser);
 											<td class='list_description'>
 												<form method='POST' action='?perfil=evento'>
 													<input type='hidden' name='apagar' value='".$campo['id']."' />
-													<input type ='submit' class='btn btn-theme  btn-block' value='apagar'>
+													<button class='btn btn-theme' type='button' data-toggle='modal' data-target='#confirmApagar'>Apagar</button>
+													<!--<input type ='submit' class='btn btn-theme  btn-block' value='apagar' data-toggle='modal' data-target='#confirmApagar'>-->
 												</form>
 											</td>";
 									}
@@ -176,8 +180,26 @@ $usuario = recuperaDados("usuario","id",$idUser);
 							</table>";
 					}
 					?>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+                            <div class="modal fade" id="confirmApagar" role="dialog" aria-labelledby="confirmApagarLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title">Deseja excluir o evento?</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>&nbsp;</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                                            <button type="button" class="btn btn-danger" id="confirm">Remover</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
