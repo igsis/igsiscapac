652ce3c70fad00b8807cd4d5c7dbe1d3ee1c97de
Tanair <tanaiir@gmail.com>
2019-01-03 14:08:04 -0200

    Create oficineiro_pj_informacoes_complementares.php

---
+++ perfil/oficineiro_pj_informacoes_complementares.php
@@ -0,0 +1,83 @@
+<?php
+$con = bancoMysqli();
+$idPj = $_SESSION['idPj'];
+$evento = isset($_SESSION['idEvento']) ? $_SESSION['idEvento'] : null;
+
+if(isset($_POST['cadastrarJuridica']))
+{
+    $idPj = $_POST['cadastrarJuridica'];
+
+    $sql_atualiza_complementares = "UPDATE pessoa_juridica SET
+	
+	WHERE `id` = '$idPj'";
+
+    if(mysqli_query($con,$sql_atualiza_complementares))
+    {
+        $mensagem = "<font color='#01DF3A'><strong>Atualizado com sucesso!</strong></font>";
+        gravarLog($sql_atualiza_complementares);
+    }
+    else
+    {
+        $mensagem = "<font color='#FF0000'><strong>Erro ao atualizar! Tente novamente.</strong></font>";
+    }
+}
+
+$pj = recuperaDados("pessoa_juridica","id",$idPj);
+$evento_pj = recuperaDados("evento","id",$evento);
+
+?>
+
+<section id="contact" class="home-section bg-white">
+    <div class="container"><?php include 'includes/menu_interno_pj.php'; ?>
+        <div class="form-group">
+            <h3>INFORMAÇÕES COMPLEMENTARES</h3>
+            <p><b>Código de cadastro:</b> <?php echo $idPj; ?> | <b>Razão Social:</b> <?php echo $pj['razaoSocial']; ?></p>
+            <h5><?php if(isset($mensagem)){echo $mensagem;};?></h5>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <form class="form-horizontal" role="form" action="?perfil=informacoes_complementares_pj" method="post">
+
+
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-8">
+                            <input type="hidden" name="cadastrarJuridica" value="<?php echo $idPj ?>">	<input type="hidden" name="Sucesso" id="Sucesso" />
+                            <input type="submit" value="GRAVAR" class="btn btn-theme btn-lg btn-block">
+                        </div>
+                    </div>
+                </form>
+
+                <!-- Botão para Voltar e Prosseguir -->
+                <div class="form-group">
+                    <div class="col-md-offset-2 col-md-2">
+                        <form class="form-horizontal" role="form" action="?perfil=endereco_pj" method="post">
+                            <input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPj ?>">
+                        </form>
+                    </div>
+                    <?php
+                    if ($evento_pj['contratacao'] == 2)
+                    {
+                        ?>
+                        <div class="col-md-offset-4 col-md-2">
+                            <form class="form-horizontal" role="form" action="?perfil=anexos_pj" method="post">
+                                <input type="submit" value="Avançar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPf ?>">
+                            </form>
+                        </div>
+                        <?php
+                    }
+                    else
+                    {
+                        ?>
+                        <div class="col-md-offset-4 col-md-2">
+                            <form class="form-horizontal" role="form" action="?perfil=dados_bancarios_pj" method="post">
+                                <input type="submit" value="Avançar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPj ?>">
+                            </form>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
