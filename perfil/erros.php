cb500393d508819b5ebdb75fbaddaf6b07fdd50b
Diego Vicente <diego.vdossantos@gmail.com>
2019-01-14 17:16:46 -0200

    correção pj oficinas

--- perfil/erros.php
+++ perfil/erros.php
@@ -89,7 +89,27 @@ switch($p)
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=representante2_pj'>";
 	break;
 
-	case 'erro_artista_pj':
+    case 'erro_representante3':
+        ?>
+        <section id="list_items" class="home-section bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-offset-2 col-md-8">
+                        <div class="section-heading">
+                            <h4><font color='red'>CPF inválido! por favor, insira o número correto!</font></h4>
+                            <h4><font color='red'>Redirecionando...</font></h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php
+        echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=oficineiro_pj_representante'>";
+        break;
+
+
+    case 'erro_artista_pj':
 		$id_ped = $_GET['id_ped'];
 	?>
 		<section id="list_items" class="home-section bg-white">
