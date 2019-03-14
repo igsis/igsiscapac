97f2ac741089c44c3cd4bfe1b1efdc14dcf7be8b
Lorelei Gabriele <loreleigab@hotmail.com>
2019-03-14 16:54:19 -0300

    Update email.php

--- email.php
+++ email.php
@@ -31,7 +31,7 @@ if (isset($_POST['enviarEmail']))
 
         // To send HTML mail, the Content-type header must be set
         $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 
         // Create email headers
         $headers .= "De: no.reply.smcsistemas@gmail.com\r\n";
