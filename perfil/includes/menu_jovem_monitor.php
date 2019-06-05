9bf7e6b4ca899d0a9cfc79d3c69ee7827ee0e886
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-06-05 11:25:27 -0300

    escondendo o enviar arquivo caso ele nao tenha realizado o cadastro de jovem monitor

--- perfil/includes/menu_jovem_monitor.php
+++ perfil/includes/menu_jovem_monitor.php
@@ -3,7 +3,13 @@
         <div class="col-md-offset-2 col-md-8">
             <strong>
                 | <a href="?secao=perfil">Início</a>
-                | <a href="?perfil=jovem_monitor/arquivos">Inserir arquivos</a>
+                <?php
+                    if($idJovemMonitor != null){
+                        ?>
+                        | <a href="?perfil=jovem_monitor/arquivos">Inserir arquivos</a>
+                <?php
+                    }
+                ?>
                 | <a href="http://smcsistemas.prefeitura.sp.gov.br/igsiscapac/manual/" target="_blank">Ajuda</a>
                 | <a href="../include/logoff.php">Sair</a> |</strong><br/>
         </div>
