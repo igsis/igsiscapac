004bd0694fcb3493b9749645fbd54357d1eff7e7
Diego Vicente <diego.vdossantos@gmail.com>
2019-06-18 14:07:20 -0300

    emenda - parceria: arquivos

--- #modelagem/alteracao_emenda.sql
+++ #modelagem/alteracao_emenda.sql
@@ -8,4 +8,23 @@ CREATE TABLE `emenda_parlamentar` (
   PRIMARY KEY (`id`)
 );
 
-asINSERT INTO `upload_lista_documento` (`id`, `idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES (165, 2, 'Plano de Trabalho', 'planot', 1, 1, 1, 1, NULL, 1);
+ALTER TABLE `upload_lista_documento` CHANGE COLUMN `publicado` `publicado` TINYINT(1) NULL DEFAULT '1' AFTER `validade`;
+
+INSERT INTO `upload_lista_documento` (`id`, `idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES (165, 2, 'Plano de Trabalho', 'planot', 1, 1, 1, 1, NULL, 1);
+
+INSERT INTO `upload_tipo` (`id`, `tipo`) VALUES ('8', 'emenda_parceria');
+
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES (8, 'Ofício de Solicitação de Parceria', 'parc_of', 0, 0, 0, 0, NULL, 1);
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES (8, 'Comprovante de Endereço da Instituição Proponente', 'parc_end', 0, 0, 0, 0, NULL, 1);
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Formulário de Abertura de Conta Corrente no Banco do Brasil', 'parc_bb', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração De Inexistência De Débitos Perante A Fazenda SP', 'parc_deb', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração De Não Emprego De Menores', 'parc_men', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração de Ficha Limpa', 'parc_fl', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração De Inexistência De Impedimentos', 'parc_inex', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração de Não Servidor Público', 'parc_serv', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Declaração de Ciência; Carta de Autorização de Abertura de Contas; Inscrição no CNPJ – Receita Federal', 'parc_cnpj', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Contrato Social / Estatuto / Requerimento de Empresário', 'parc_cs', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Cópia da ATA da última reunião de eleição de Diretoria', 'parc_ata1', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Cópia da ATA da última reunião para os demais casos', 'parc_ata2', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Relação nominal atualizada dos dirigentes da entidade', 'parc_diri', '0', '0', '0', '0', NULL, '1');
+INSERT INTO `upload_lista_documento` (`idTipoUpload`, `documento`, `sigla`, `teatro`, `musica`, `oficina`, `edital`, `validade`, `publicado`) VALUES ('8', 'Currículo da entidade/empresa', 'parc_curri', '0', '0', '0', '0', NULL, '1');
