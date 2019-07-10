58dfa8a9d5c203c02084367c3709c961e39617f3
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-07-10 16:55:10 -0300

    Alguns ajustes na tela de cadastro de jm

--- perfil/jovem_monitor/cadastro.php
+++ perfil/jovem_monitor/cadastro.php
@@ -21,8 +21,8 @@ if(isset($_POST['cadastrar']) || isset($_POST['editar'])){
     $email = $_POST['email'];
 
     if(isset($_POST['cadastrar'])){
-        $sql = "INSERT INTO pessoa_fisica (nome, nomeArtistico, rg, cpf, dataNascimento, bairro, cidade, estado, cep, numero, complemento, telefone1, email, jovem_monitor, idUsuario)
-                VALUES ('$nome','$nomeArtistico', '$rg', '$cpf', '$dataNascimento','$bairro', '$cidade', '$estado', '$cep', '$numero', '$complemento', '$telefone', '$email', 1, '$idUser')";
+        $sql = "INSERT INTO pessoa_fisica (nome, nomeArtistico, rg, cpf, dataNascimento, logradouro,bairro, cidade, estado, cep, numero, complemento, telefone1, email, jovem_monitor, idUsuario)
+                VALUES ('$nome','$nomeArtistico', '$rg', '$cpf', '$dataNascimento','$logradouro','$bairro', '$cidade', '$estado', '$cep', '$numero', '$complemento', '$telefone', '$email', 1, '$idUser')";
 
     }else{
         $sql = "UPDATE pessoa_fisica SET 
@@ -31,13 +31,14 @@ if(isset($_POST['cadastrar']) || isset($_POST['editar'])){
                 rg = '$rg', 
                 cpf = '$cpf', 
                 dataNascimento = '$dataNascimento', 
+                logradouro = '$logradouro',
                 bairro = '$bairro', 
                 cidade = '$cidade', 
-                estado = '$estado'
+                estado = '$estado',
                 cep = '$cep', 
                 numero = '$numero', 
                 complemento = '$complemento', 
-                 telefone1 = '$telefone', 
+                 telefone1 = '$telefone' 
                 WHERE idUsuario = '$idUser'";
     }
 
@@ -83,7 +84,7 @@ $idJovemMonitor = $jovem_monitor['idUsuario'] ?? null;
                         <form method="POST" action="?perfil=jovem_monitor/cadastro" class="form-horizontal"
                               role="form">
                             <div class="form-group">
-                                <label for="nome">Nome Completo </label>
+                                <label for="nome">Nome Completo *</label>
                                 <input type="text" name="nome" class="form-control" id="nome" readonly
                                        value="<?= $cadastro_basico['nome'] ?>" required>
                             </div>
@@ -95,33 +96,33 @@ $idJovemMonitor = $jovem_monitor['idUsuario'] ?? null;
                             </div>
 
                             <div class="form-group">
-                                <label>Data Nascimento </label>
+                                <label>Data Nascimento *</label>
                                 <input type="text" class="form-control" name="dataNascimento" id="datepicker01"
                                        placeholder="Data de Nascimento" required
                                        value="<?= ($idJovemMonitor == null) ? "" : exibirDataBr($jovem_monitor['dataNascimento']) ?>">
                             </div>
 
                             <div class="form-group">
-                                <label>RG </label>
+                                <label>RG *</label>
                                 <input type="text" name="rg" class="form-control" id="rg"
                                        value="<?= $jovem_monitor['rg'] ?>" required>
                             </div>
 
                             <div class="form-group">
-                                <label>CPF </label>
+                                <label>CPF *</label>
                                 <input type="text" name="cpf" class="form-control" id="cpf"
                                        value="<?= $jovem_monitor['cpf'] ?>" data-mask="000.000.000-00" required>
                             </div>
 
                             <div class="form-group">
-                                <label>Email </label>
+                                <label>Email *</label>
                                 <input type="text" name="email" class="form-control" id="email"
                                        value="<?= $cadastro_basico['email'] ?>" data-mask="000.000.000-00" required
                                        readonly>
                             </div>
 
                             <div class="form-group">
-                                <label>Telefone </label>
+                                <label>Telefone *</label>
                                 <input type="text" class="form-control" name="telefone" id="telefone"
                                        onkeyup="mascara( this, mtel );" maxlength="15"
                                        placeholder="Exemplo: (11) 98765-4321" required
@@ -135,7 +136,7 @@ $idJovemMonitor = $jovem_monitor['idUsuario'] ?? null;
                             </div>
 
                             <div class="form-group">
-                                <strong>Endereço:</strong>
+                                <strong>Endereço: *</strong>
                                 <input type="text" readonly class="form-control" id="Endereco" name="Endereco"
                                        placeholder="Endereço" value="<?= $jovem_monitor['logradouro'] ?>" required>
                             </div>
@@ -153,18 +154,18 @@ $idJovemMonitor = $jovem_monitor['idUsuario'] ?? null;
                             </div>
 
                             <div class="form-group">
-                                <strong>Bairro:</strong>
+                                <strong>Bairro: *</strong>
                                 <input type="text" readonly class="form-control" id="Bairro" name="Bairro"
                                        placeholder="Bairro" value="<?= $jovem_monitor['bairro'] ?>">
                             </div>
 
                             <div class="form-group">
-                                <strong>Cidade:</strong>
+                                <strong>Cidade: *</strong>
                                 <input type="text" readonly class="form-control" id="Cidade" name="Cidade"
                                        placeholder="Cidade" value="<?= $jovem_monitor['cidade'] ?>">
                             </div>
                             <div class="form-group">
-                                <strong>Estado:</strong>
+                                <strong>Estado: *</strong>
                                 <input type="text" readonly class="form-control" id="Estado" name="Estado"
                                        placeholder="Estado" value="<?= $jovem_monitor['estado'] ?>">
                             </div>
